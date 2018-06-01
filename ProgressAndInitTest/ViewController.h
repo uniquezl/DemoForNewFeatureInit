ced8ab982484ff736879516eb9ce7c86fb83e627
uniquezl <zhulin_8256@sina.cn>
2018-06-01 15:48:46 +0800

    Initial Commit

---
+++ ProgressAndInitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressAndInitTest
+//
+//  Created by zhulin on 2018/6/1.
+//  Copyright © 2018年 zhulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProgressAndInitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProgressAndInitTest
+//
+//  Created by zhulin on 2018/6/1.
+//  Copyright © 2018年 zhulin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
